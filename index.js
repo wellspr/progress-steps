70b3f71a27b10d6feb7817d3d91ebf9275357d46
Paulo Wells <paulowells@gmail.com>
2023-07-04 17:12:31 -0300

    added steps structure

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("ok")
+const steps = document.querySelectorAll(".step");
+const connectors = document.querySelectorAll(".connector");
+
+let stepLabel = 0;
+
+steps.forEach(step => {
+    stepLabel++;
+    step.innerHTML = String(stepLabel);
+    step.classList.add("disabled");
+});
+
+connectors.forEach(con => {
+    con.classList.add("disabled");
+});
+
+steps[0].classList.remove("disabled");
